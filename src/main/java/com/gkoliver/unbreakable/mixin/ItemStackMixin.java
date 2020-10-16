e1a93a81c42303d4bbb9127fa552c2688f64a15e
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-10-16 14:48:15 -0500

    Trying to fix issues with the mixin's function

--- src/main/java/com/gkoliver/unbreakable/mixin/ItemStackMixin.java
+++ src/main/java/com/gkoliver/unbreakable/mixin/ItemStackMixin.java
@@ -3,12 +3,13 @@ package com.gkoliver.unbreakable.mixin;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 
 import javax.annotation.Nullable;
 import java.util.Random;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
+    @Overwrite
     public boolean attemptDamageItem(int amount, Random rand, @Nullable ServerPlayerEntity damager) { return false; }
-
 }
